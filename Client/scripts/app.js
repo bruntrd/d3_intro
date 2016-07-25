fb0f065b59e8ebec8ad48ded879db48ddec370f6
bruntrd <bruntrd@gmail.com>
2016-07-24 22:29:03 -0500

    labels

--- Client/scripts/app.js
+++ Client/scripts/app.js
@@ -2,7 +2,7 @@
 var w = 500;
 var h = 200;
 var padding = 5;
-var dataSet = [5,10,15,20,25];
+var dataSet = [5,10,15,20,25,12,15,24,31,8,42];
 var svg = d3.select("body")
     .append("svg")
     .attr("width", w)
@@ -31,6 +31,16 @@ svg.selectAll("rect")
     .attr("fill", function(d){
         return "rgb(0, "+ (d*10) + ", 0)";
     });
+
+svg.selectAll('text')
+    .data(dataSet)
+    .enter()
+    .append("text")
+    .text(function(d){return d;})
+    .attr("text-anchor", "middle")
+    .attr('x',function(d,i){return i * (w/ dataSet.length)+(w/dataSet.length-padding)/2;})
+    .attr('y', function(d) {return h-(d*4)+10})
+    .attr('fill','white');
 
 //svg.selectAll("rect")
 //    .data(dataSet)
--- Server/public/assets/scripts/app.js
+++ Server/public/assets/scripts/app.js
@@ -2,14 +2,14 @@
 var w = 500;
 var h = 200;
 var padding = 5;
-var dataSet = [5,10,15,20,25];
+var dataSet = [5,10,15,20,25,12,15,24,31,8,42];
 var svg = d3.select("body")
     .append("svg")
     .attr("width", w)
     .attr("height",h);
 
 var maxH = Math.max.apply(Math,dataSet);
-console.log(maxH)
+//console.log(maxH)
 
 //d3 can use d as the data set value as d and i as the index of that one
 svg.selectAll("rect")
@@ -31,6 +31,16 @@ svg.selectAll("rect")
     .attr("fill", function(d){
         return "rgb(0, "+ (d*10) + ", 0)";
     });
+
+svg.selectAll('text')
+    .data(dataSet)
+    .enter()
+    .append("text")
+    .text(function(d){return d;})
+    .attr("text-anchor", "middle")
+    .attr('x',function(d,i){return i * (w/ dataSet.length)+(w/dataSet.length-padding)/2;})
+    .attr('y', function(d) {return h-(d*4)+10})
+    .attr('fill','white');
 
 //svg.selectAll("rect")
 //    .data(dataSet)
