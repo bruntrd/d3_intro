cd20191d51b0ed93e4c5106d3ff89eec34f2a68d
bruntrd <bruntrd@gmail.com>
2016-07-27 21:06:23 -0500

    more scatter plots

--- Server/public/assets/scripts/app.js
+++ Server/public/assets/scripts/app.js
@@ -71,6 +71,39 @@ var svg = d3.select("body").append("svg").attr({ width:w, height: h});
 
 
 //add dots
+//KPI color
+function salesKPI (d) {
+    if (d>=250) { return "#33CC66"; } else
+    if (d<250) { return "#666666"; }
+}
+
+//create our SVG
+var svg = d3.select("body").append("svg").attr({ width:w, height: h});
+
+//add min/max to array
+Array.max = function( array ){
+    return Math.max.apply( Math, array );
+};
+
+Array.min = function( array ){
+    return Math.min.apply( Math, array );
+};
+
+//function for showing labels
+function showMinMax(ds, col, val, type){
+    var max = d3.max(ds, function(d) { return d[col]; } );
+    var min = d3.min(ds, function(d) { return d[col]; } );
+
+    if (type=='minmax' && (val == max || val == min)) {
+        return val;
+    } else
+
+    if (type=='all') {
+        return val;
+    }
+
+}
+//add dots
 var dots = svg.selectAll("circle")
     .data(monthlySales)
     .enter()
@@ -79,7 +112,21 @@ var dots = svg.selectAll("circle")
         cx: function(d){ return d.month*3; },
         cy: function(d){ return h-d.sales; },
         r:  5,
-        "fill": "#666666"
+        "fill": function(d){ return salesKPI(d.sales); }
+    });
+
+var labels = svg.selectAll("text")
+    .data(monthlySales)
+    .enter()
+    .append("text")
+    .text(function(d){ return showMinMax(monthlySales, 'sales', d.sales, 'minmax'); })
+    .attr({
+        x: function(d){ return (d.month*3)-25; },
+        y: function(d){ return h-d.sales; },
+        "font-size": "12px",
+        "font-family": "sans-serif",
+        "fill": "#666666",
+        "text-anchor": "start"
     });
 
 
